7e8b1b35975f20694dbcde6ee2dbd456f47db77c
hajime.takeuchi <shadowthe8156@gmail.com>
2025-02-25 22:48:37 +0900

    add comments

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
+// 入力テキストボックス
 const todoInputText = document.getElementById("input_todo");
 
+// 追加ボタン
 const todoAddButton = document.getElementById("add_todo");
 
+// TODO一覧
 const todoList = document.getElementById("todo_list");
 
+// 追加ボタンクリック時のイベントを登録
 todoAddButton.addEventListener('click', () => {
   console.log(todoInputText.value);
 });
 
+// liを作成します
 const listItem = document.createElement("li");
-listItem.className = "todo-li";
-listItem.innerHTML = "hogehoge";
+listItem.className = "todo-li";  // liのclassを設定します
+listItem.innerHTML = "hogehoge"; // liの中身を設定します
 
+// buttonを作成します
 const deleteButton = document.createElement("button");
-deleteButton.innerHTML = "DELETE";
-deleteButton.type = "button";
+deleteButton.innerHTML = "DELETE"; // buttonのclassを設定します
+deleteButton.type = "button";      // buttonの中身を設定します
 
+// liの子要素として、buttonを追加します
 listItem.appendChild(deleteButton);
+// ulの子要素として、liを追加します
 todoList.appendChild(listItem);
