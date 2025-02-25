6b23c9c5581dc1a2e0ded10e7baad20407ca161f
hajime.takeuchi <shadowthe8156@gmail.com>
2025-02-25 21:47:35 +0900

    textboxの内容をconsoleに表示する

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello!");
+const todoInputText = document.getElementById("input_todo");
+
+const todoAddButton = document.getElementById("add_todo");
+
+todoAddButton.addEventListener('click', () => {
+  console.log(todoInputText.value);
+});
