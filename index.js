4448ced174a005dad5c702af5d2ec888a72a8a96
hajime.takeuchi <shadowthe8156@gmail.com>
2025-02-25 22:45:00 +0900

    add todoli logic

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const todoInputText = document.getElementById("input_todo");
 
 const todoAddButton = document.getElementById("add_todo");
 
+const todoList = document.getElementById("todo_list");
+
 todoAddButton.addEventListener('click', () => {
   console.log(todoInputText.value);
 });
+
+const listItem = document.createElement("li");
+listItem.className = "todo-li";
+listItem.innerHTML = "hogehoge";
+
+const deleteButton = document.createElement("button");
+deleteButton.innerHTML = "DELETE";
+deleteButton.type = "button";
+
+listItem.appendChild(deleteButton);
+todoList.appendChild(listItem);
